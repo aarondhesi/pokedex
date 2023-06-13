7299ccf6ecc0b95d79488ddf060fec0590c5b272
Aaron Dhesi <74552980+aarondhesi@users.noreply.github.com>
2023-06-13 21:01:07 +0100

    Add heading to pokedex entry

---
+++ src/lib/Heading.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let pokemonID;
+  export let pokemonName;
+
+
+  function generatePokemonNumber(id)
+  {
+    if (id >= 100)
+    {
+      return id.toString();
+    }
+    else if (id >= 10)
+    {
+      return "0" + id.toString();
+    }
+    else
+    {
+      return "00" + id.toString();
+    }
+  }
+
+  $: pokemonNumber = generatePokemonNumber(pokemonID);
+</script>
+
+<h1>#{pokemonNumber} {pokemonName}</h1>
--- src/lib/PokedexEntry.svelte
+++ src/lib/PokedexEntry.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Heading from "./Heading.svelte";
   import Image from "./Image.svelte";
   export let pokemonData;
   console.log(pokemonData);
 </script>
 
+<Heading pokemonID={pokemonData.id} pokemonName={pokemonData.name}  />
 <Image pokemonID={pokemonData.id} pokemonName={pokemonData.name} />
