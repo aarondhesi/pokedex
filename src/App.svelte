8b63ba434ff2649fd0b923eafc5822c57c08621d
Aaron Dhesi <74552980+aarondhesi@users.noreply.github.com>
2023-06-18 12:31:08 +0100

    Add blank Navigation Bar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import PokedexEntry from "./lib/PokedexEntry.svelte";
+  import NavigationBar from "./lib/NavigationBar.svelte";
   import data from "./data/pokemon.json";
 </script>
 
+<NavigationBar />
 <PokedexEntry pokemonData={data} />
