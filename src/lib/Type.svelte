781276196eed501c0c7c245b7c00dd82ca8ff419
Aaron Dhesi <74552980+aarondhesi@users.noreply.github.com>
2023-07-07 20:53:06 +0100

    Add Type component
    - Displaying Pokemon Type

---
+++ src/lib/Type.svelte
@@ -0,0 +1,31 @@
+<script>
+  import types from "../data/types.json";
+
+  function getTypeColour(type) {
+    for (let i = 0; i <= types.length; i++) {
+      let object = types[i];
+
+      if (object.type === type) {
+        return object.colour;
+      }
+    }
+  }
+
+  export let type;
+  let colour = getTypeColour(type);
+</script>
+
+<p style="background-color: {colour}">{type}</p>
+
+<style>
+  p {
+    color: black;
+    font-family: monospace;
+    font-size: 16px;
+    width: 100px;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 10px;
+  }
+</style>
