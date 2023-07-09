0b0091eaedfb939b3ee2187712821ed4f828d795
Aaron Dhesi <74552980+aarondhesi@users.noreply.github.com>
2023-07-09 14:22:22 +0100

    Add Pokedex Entry component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,12 @@
 <script>
   import DarkModeToggle from "./lib/DarkModeToggle.svelte";
   import NavTileGrid from "./lib/NavTileGrid.svelte";
+
   import allPokemon from "./data/pokemon.json";
-  import Type from "./lib/Type.svelte";
 </script>
 
 <div class="grid-container">
   <div class="header">
-    <Type type="Electric" />
     <DarkModeToggle />
   </div>
   <NavTileGrid pokemon={allPokemon} />
---
+++ src/lib/PokedexEntry.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let pokemon;
+  import generatePokemonNumber from "../scripts/generatePokedexNumberString";
+  import Type from "./Type.svelte";
+</script>
+
+<main>
+  <p>{generatePokemonNumber(pokemon.number)} {pokemon.name}</p>
+  <p>Category: {pokemon.category}</p>
+  {#each pokemon.type as type}
+    <Type {type} />
+  {/each}
+  <p>{pokemon.height} m</p>
+  <p>{pokemon.weight} kg</p>
+  <p>Ability: {pokemon.ability}</p>
+</main>
+
+<style>
+  main {
+    margin: auto;
+    max-width: 1090px;
+    background-color: #a6a6bf;
+    font-family: monospace;
+    font-size: 16px;
+  }
+</style>
